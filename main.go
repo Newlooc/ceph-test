e800562e5f36a0cea6df3ef60334cb0112903dff
lechuan.niu <lechuan.niu@daocloud.io>
2020-06-18 11:55:47 +0800

    Get pool

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
 	cephutil "github.com/ceph/ceph-csi/pkg/util"
-	//rbd "github.com/ceph/go-ceph/rbd"
+	rbd "github.com/ceph/go-ceph/rbd"
 	"time"
 )
 
@@ -12,5 +13,26 @@ func main() {
 
 	connPool := cephutil.NewConnPool(2*time.Second, 10*time.Second)
 	conn, _ := connPool.Get("kubernetes", "10.6.209.21:6789", cred.ID, cred.KeyFile)
+
+	info, _ := conn.GetClusterStats()
+
+	fmt.Printf("%+v\n", info)
+
+	pools, _ := conn.ListPools()
+
+	for _, pool := range pools {
+		ctx, _ := conn.OpenIOContext(pool)
+		ims, _ := rbd.GetImageNames(ctx)
+		for _, im := range ims {
+			fmt.Printf("=====================%s==================\n", im)
+			imobj, _ := rbd.OpenImageReadOnly(ctx, im, rbd.NoSnapshot)
+			info, err := imobj.Stat()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			fmt.Printf("%+v\n", info)
+			fmt.Println("=======================================")
+		}
+	}
 
 }
